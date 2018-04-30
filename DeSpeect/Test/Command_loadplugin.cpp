4925db15b0b8393ca80d0094972cab4a70d6f0b7
Manfredi Smaniotto <manfri96@live.it>
2018-04-30 15:51:08 +0200

    aggiunta test loadplugin

--- DeSpeect/Test/Command_loadplugin.cpp
+++ DeSpeect/Test/Command_loadplugin.cpp
@@ -1,10 +1,9 @@
 #include "gtest/gtest.h"
-#include "../Model/Command/header/setspeectconfigcommand.h"
-#include "../Model/SpeectWrapper/header/Speect.h"
-#include "../Model/Command/header/uttprocessorcommand.h"
-#include "../Model/Command/header/commandlist.h"
+#include "../Model/Command/header/loadplugincommand.h"
 
 TEST(Command, LoadPluginTest){
-
-
+    Speect* s=new Speect();
+    std::string str("utt_textgrid.spi");
+    LoadPluginCommand* cpl=new LoadPluginCommand(str);
+    EXPECT_EQ(cpl->execute(s), "Adding Plugin:utt_textgrid.spi Operation status:No error");
 }
