3a33bdc65fe9f482dc1535f4f4959cf414e51da5
Unknown <cristiano.tessarolo@studenti.unipd.it>
2018-05-01 18:36:33 +0200

    Add test node

--- DeSpeect/Test/View_arcTest.cpp
+++ DeSpeect/Test/View_arcTest.cpp
@@ -2,22 +2,6 @@
 #include <QApplication>
 #include "arc.h"
 
-#include <commandlist.h>
-#include <unistd.h>
-//#define GetCurrentDir getcwd
-
-#include "ui_view.h"
-#include "modelview.h"
-#include <QFileDialog>
-#include <graphmanager.h>
-#include <loadplugincommand.h>
-#include <setspeectconfigcommand.h>
-#include <uttprocessorcommand.h>
-#include "relation.h"
-#include "QTextStream"
-#include <QFont>
-#include <QProcess>
-
 #include <QPainter>
 #include <QWidget>
 #include <QStyleOptionGraphicsItem>
@@ -48,5 +32,30 @@ TEST(Graph, VerifyArc){
     delete option;
 
 
+}
+
+
+TEST(Graph, VerifyArcParam){
+    int argc;
+    char **argv=NULL;
+    QApplication app(argc,argv);
+    QPainter* p = new QPainter();
+    QWidget* temp= new QWidget();
+    QColor colore(255,255,255);
+    const int Radius=1;
+    Arc* a1;
+    a1=new Arc(12,Radius,colore,0,1,NULL,0);
+    a1->UpdateStartPoint(QPoint(0,0));
+    a1->UpdateEndPoint(QPoint(10,10));
+
+    QStyleOptionGraphicsItem *option = new QStyleOptionGraphicsItem();
+    a1->paint(p,option,temp);
+
+    EXPECT_TRUE(a1);
+
+    delete a1;
+    delete p;
+    delete temp;
+    delete option;
 }
 
---
+++ DeSpeect/Test/View_nodeTest.cpp
@@ -0,0 +1,129 @@
+#include "gtest/gtest.h"
+#include <QApplication>
+#include "node.h"
+#include "arc.h"
+
+#include <QPainter>
+#include <QWidget>
+#include <QStyleOptionGraphicsItem>
+#include <QColor>
+#include "item.h"
+
+//#include <QtTest/QTest>
+TEST(Graph, VerifyNodePaint){
+    int argc;
+    char **argv=NULL;
+    QApplication app(argc,argv);
+    QPainter* p = new QPainter();
+    QWidget* temp= new QWidget();
+
+    Node* n = new Node("n1","n1","n1",0,0,10,QColor(255,0,0),NULL);
+
+    QStyleOptionGraphicsItem *option = new QStyleOptionGraphicsItem();
+    n->paint(p,option,temp);
+
+    EXPECT_TRUE(n);
+
+    delete n;
+    delete p;
+    delete temp;
+    delete option;
+
+
+}
+
+TEST(Graph, VerifyNodePaintSelected){
+    int argc;
+    char **argv=NULL;
+    QApplication app(argc,argv);
+    QPainter* p = new QPainter();
+    QWidget* temp= new QWidget();
+
+    Node* n = new Node("n1","n1","n1",0,0,10,QColor(255,0,0),NULL);
+
+    n->setSelected(true);
+
+    QStyleOptionGraphicsItem *option = new QStyleOptionGraphicsItem();
+    n->paint(p,option,temp);
+
+    EXPECT_TRUE(n);
+
+    delete n;
+    delete p;
+    delete temp;
+    delete option;
+
+
+}
+
+TEST(Graph, VerifyNodePaintFocus){
+    int argc;
+    char **argv=NULL;
+    QApplication app(argc,argv);
+    QPainter* p = new QPainter();
+    QWidget* temp= new QWidget();
+
+    Node* n = new Node("n1","n1","n1",0,0,10,QColor(255,0,0),NULL);
+
+    n->setFocus();
+
+    QStyleOptionGraphicsItem *option = new QStyleOptionGraphicsItem();
+    n->paint(p,option,temp);
+
+    EXPECT_TRUE(n);
+
+    delete n;
+    delete p;
+    delete temp;
+    delete option;
+
+
+}
+
+TEST(Graph, VerifyNodeEqNode){
+    int argc;
+    char **argv=NULL;
+    QApplication app(argc,argv);
+
+    Node n1("n1","n1","n1",0,0,10,QColor(255,0,0),NULL);
+    Node n2("n2","n2","n2",30,30,10,QColor(255,255,0),NULL);
+
+    EXPECT_FALSE(n1==n2);
+
+}
+
+TEST(Graph, VerifyNodeEqNodeID){
+    int argc;
+    char **argv=NULL;
+    QApplication app(argc,argv);
+
+    Node n1("n1","n1","n1",0,0,10,QColor(255,0,0),NULL);
+    ID idN1("n1","n1");
+
+    EXPECT_TRUE(n1==idN1);
+
+}
+/*
+TEST(Graph, VerifyNodeMouseRelease){
+    int argc;
+    char **argv=NULL;
+    QApplication app(argc,argv);
+
+    Node* n = new Node("n1","n1","n1",0,0,10,QColor(255,0,0),NULL);
+    QTest::mouseRelease(n,Qt::LeftButton);
+
+    EXPECT_TRUE(n);
+
+}*/
+
+TEST(Graph, VerifyNodeCatchVisibility){
+    int argc;
+    char **argv=NULL;
+    QApplication app(argc,argv);
+
+    Node* n = new Node("n1","n1","n1",0,0,10,QColor(255,0,0),NULL);
+    n->catchVisibilityChange();
+
+    EXPECT_TRUE(n);
+
+}
