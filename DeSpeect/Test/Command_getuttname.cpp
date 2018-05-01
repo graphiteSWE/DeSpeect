9835cda460715bea9524d8451ffb249d441c575a
Unknown <cristiano.tessarolo@studenti.unipd.it>
2018-05-01 11:09:35 +0200

    Auto stash before merge of "master" and "origin/master"

--- DeSpeect/Test/Command_getuttname.cpp
+++ DeSpeect/Test/Command_getuttname.cpp
@@ -9,8 +9,12 @@ TEST(Command, getuttnamesuccess){
     Speect* s=new Speect();
     CommandList::CommandBuilder* builder=new CommandList::CommandBuilder(s);
     CommandList* commands;
+    commands=builder->getCommandList();
+    commands->getRelationNames();
+    commands->getRelation("");
     commands=builder->LoadConfig(Configuration::Voice, "./cmu_arctic_slt/voice.json").getCommandList();
     commands->executeAll();
+    commands->clearErrorState();
     std::string name=commands->getUttTypeNames().begin()->c_str();
     EXPECT_EQ(name, "text");
     delete s;
