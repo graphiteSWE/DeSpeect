2c8a94f59ff7e0cc7553a67e48e0b5e4071055ab
Unknown <cristiano.tessarolo@studenti.unipd.it>
2018-05-01 11:45:39 +0200

    Cambiamento test ID

--- DeSpeect/Test/View_idTest.cpp
+++ DeSpeect/Test/View_idTest.cpp
@@ -2,9 +2,7 @@
 #include "id.h"
 
 TEST(Graph, VerifyId){
-    ID* a = new ID("a","a");
-    ID* b = new ID("b","b");
+    ID a("a","a");
+    ID b("b","b");
     EXPECT_FALSE(a==b);
-    delete a;
-    delete b;
 }
