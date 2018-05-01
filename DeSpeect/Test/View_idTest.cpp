76a304b8a811b6aff06ae59365e472e1a4c0c3df
Unknown <cristiano.tessarolo@studenti.unipd.it>
2018-05-01 17:17:52 +0200

    Add test Id

--- DeSpeect/Test/View_idTest.cpp
+++ DeSpeect/Test/View_idTest.cpp
@@ -6,3 +6,15 @@ TEST(Graph, VerifyId){
     ID b("b","b");
     EXPECT_FALSE(a==b);
 }
+
+TEST(Graph, VerifyIdEq){
+    ID a("a","a");
+    ID b=a;
+    EXPECT_TRUE(a==b);
+}
+
+TEST(Graph, VerifyIdEq2){
+    ID a("a","a");
+    a=a;
+    EXPECT_TRUE(a==a);
+}
