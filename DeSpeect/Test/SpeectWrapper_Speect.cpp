807e26cf1a59244572a673a383f440dd7fce2463
Marco Focchiatti <graphite.swe@gmail.com>
2018-05-03 15:33:28 +0200

    New test speect

--- DeSpeect/Test/SpeectWrapper_Speect.cpp
+++ DeSpeect/Test/SpeectWrapper_Speect.cpp
@@ -92,3 +92,17 @@ TEST(SpeectWrapper,FetchNodeOutOfBoundPath){
     delete builder;
     delete sTest;
 }
+TEST(SpeectWrapper,FetchNodeOutOfBoundPathReinitializedUtt){
+    Speect* sTest=new Speect();
+    CommandList::CommandBuilder* builder=new CommandList::CommandBuilder(sTest);
+    builder->LoadConfig(Configuration::Voice,"./cmu_arctic_slt/voice.json").getCommandList()->executeAll();
+    builder->LoadConfig(Configuration::UtteranceText,"hi everybody").getCommandList()->executeAll();
+    builder->LoadConfig(Configuration::UtteranceText,"hi everybody Jonny").getCommandList()->executeAll();
+    std::list<std::string> l;
+    l.push_back("Tokenize");
+    l.push_back("Normalize");
+    builder->WithProcessors(l).getCommandList()->executeAll();
+    EXPECT_TRUE(sTest->getNode(" .n.n.n.n.n.n.n","Tokenas").size()<=2);
+    delete builder;
+    delete sTest;
+}
