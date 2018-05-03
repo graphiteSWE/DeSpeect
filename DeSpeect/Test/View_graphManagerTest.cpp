1459ab6033aaf8d22fa5934d5387298640faa78b
Cristiano <cristiano.tessarolo@studenti.unipd.it>
2018-05-03 17:35:54 +0200

    Add graphManager Test

---
+++ DeSpeect/Test/View_graphManagerTest.cpp
@@ -0,0 +1,43 @@
+#include "gtest/gtest.h"
+#include <QApplication>
+#include "arc.h"
+
+#include <QPainter>
+#include <QWidget>
+#include <QStyleOptionGraphicsItem>
+#include <QColor>
+#include "item.h"
+#include "graphmanager.h"
+
+
+TEST(Graph, VerifyChangeRelationVisibility){
+    int argc;
+    char **argv=NULL;
+    QApplication app(argc,argv);
+    GraphManager* g = new GraphManager();
+    g->changeRelationVisibility(new QStandardItem());
+    EXPECT_TRUE(g);
+
+    delete g;
+}
+
+TEST(Graph, VerifyNotifySelection){
+    int argc;
+    char **argv=NULL;
+    QApplication app(argc,argv);
+    GraphManager* g = new GraphManager();
+    g->notifySelection();
+    EXPECT_TRUE(g);
+    delete g;
+}
+
+TEST(Graph, VerifySelectItem){
+    int argc;
+    char **argv=NULL;
+    QApplication app(argc,argv);
+    GraphManager* g = new GraphManager();
+    g->selectItem("Token","");
+    EXPECT_TRUE(g);
+
+    delete g;
+}
