d4441d7d458ab46b1b944429dde0a07d08df8175
Matteo Rizzo <matteo_rizzo96@hotmail.it>
2018-05-03 16:55:12 +0200

    Parzialmente commentato Qt

--- DeSpeect/Qt/header/arc.h
+++ DeSpeect/Qt/header/arc.h
@@ -1,27 +1,67 @@
 #ifndef ARC_H
 #define ARC_H
 #include "line.h"
-
 #include <QGraphicsObject>
-//Draw a line with an arrow on end point
+
+/*
+ * File: arc.h
+ * Type: header
+ * Date: 2018-04-23
+ * E-mail: graphite.swe@gmail.com
+ * 
+ * Description: draws a line with an arrow on its end point
+ */
+ 
 class Arc:public Line
 {
+    //Macro used to enable Qt signals and slots
     Q_OBJECT
+    
 private:
-    //the triangle base = 2*arrowWidth triangle heigth arrowWidth
+    
+    /* 
+     * Field: arrow width, note that:
+     * 		  - triangle base   = 2*arrowWidth
+     * 		  - triangle heigth = arrowWidth
+     */
     const int arrowWidth;
+    
+    //Field: indicates if the arrow head is filled
     const bool isfilled;
+    
+    //Field: indicates the type of the arrow
     const bool type;
-    //point needed to draw the arrow
+    
+    //Fields: points needed to draw the arrow
     QPointF arrowPoint1;
     QPointF arrowPoint2;
-    //fix the end point and arrow points when nodes move
+    
+    //Field: fix the end point and arrow points when nodes move
     QPointF FixEnd();
+
 public:
-    //radius is needed to offset from the center
-    Arc(const int arrowWidth, const int radius, const QColor& color,bool prev_next=1,bool filled=1, QGraphicsItem*parent=0, bool dashed=0);
+
+    /*
+     * Description: Arc constructor
+     * @param const int arrowWidth - arrow width 
+     * @param const int radius - radius of the pointed nodes, it is needed to offset from the center
+     * @param const QColor& color - Qt color as the arc color (see Qt docs for more info)
+     * @param bool prev_next - indicates the kind of arc
+     * @param bool filled - indicates if the arrow head is filled
+     * @param QGraphicsItem* parent - Qt graphic item  (see Qt docs for more info)
+     * @param bool dashed - indicates if the arc is dashed
+     */
+    Arc(const int arrowWidth, const int radius, const QColor& color, bool prev_next=1, bool filled=1, QGraphicsItem*parent=0, bool dashed=0);
+
 public:
-    //redefine paint function to draw the arrow of arc
+    
+    /*
+     * Description: redefines the paint function to draw the arrow of the arc
+     * @param QPainter *painter - Qt painter (see Qt docs for more info)
+     * @param const QStyleOptionGraphicsItem *option - Qt style options for the graphic item (see Qt docs for more info)
+     * @param const QWidget *widget - Qt widget (see Qt docs for more info)
+     * @return void
+     */
     void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget);
 public slots:
 
--- DeSpeect/Qt/header/id.h
+++ DeSpeect/Qt/header/id.h
@@ -1,9 +1,17 @@
 #ifndef ID_H
 #define ID_H
-
 #include <QString>
 
 
+/*
+ * File: id.h
+ * Type: header
+ * Date: 2018-04-23
+ * E-mail: graphite.swe@gmail.com
+ * 
+ * Description: models the ID of a node. ID is the path to the phisical node from the head of his relation where relation is the relation of the phisical node. The ID is unique in a relation but not among different relations
+ */
+ 
 //ID of a node
 //id is the path to the phisical node from the head of his relation
 //relation is the relation of the phisical node
