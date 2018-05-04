fd6ee25f8beff0ebcbf5ddf4804d3045d2d00b0c
Cristiano <cristiano.tessarolo@studenti.unipd.it>
2018-05-04 14:26:30 +0200

    Add test processorManager

--- DeSpeect/Test/View_processorhManagerTest.cpp
+++ DeSpeect/Test/View_processorhManagerTest.cpp
@@ -1,6 +1,10 @@
 #include "gtest/gtest.h"
 #include <QApplication>
 #include <processormanager.h>
+
+#include <QStandardItemModel>
+#include <QStandardItem>
+#include <QListView>
 TEST(Graph, VerifyProcessorManagerIsClear){
     int argc;
     char **argv=NULL;
@@ -65,6 +69,47 @@ TEST(Graph, VerifyProcessorManagerEvidenceAll){
     p->addProcessor("d");
     p->addProcessor("e");
 
+    p->evidenceAllProcessor();
+
+    p->clearLayoutProcessor();
+
+    EXPECT_TRUE(p);
+
+    delete p;
+
+}
+
+
+
+TEST(Graph, VerifyProcessorManagerEvidenceAll2){
+    int argc;
+    char **argv=NULL;
+    QApplication app(argc,argv);
+
+    ProcessorManager *p=new ProcessorManager();
+    QListView* qList= new QListView();
+
+    p->linkProcessorModel(qList);
+
+    p->addProcessor("");
+    p->addProcessor("a");
+    p->addProcessor("b");
+    p->addProcessor("c");
+    p->addProcessor("d");
+    p->addProcessor("e");
+
+    QAbstractItemModel* qListModel=qList->model();
+
+    QStandardItemModel* model = dynamic_cast <QStandardItemModel*>(qListModel);
+
+    if(model){
+        model->item(2)->setCheckState(Qt::Unchecked);
+    }
+
+
+    p->evidenceNextProcessor();
+    p->evidenceNextProcessor();
+    p->evidenceNextProcessor();
     p->evidenceAllProcessor();
 
     p->clearLayoutProcessor();
