0c23a228ed5809d3e5322b8e8cd943e98ac8614f
Cristiano <cristiano.tessarolo@studenti.unipd.it>
2018-05-04 13:33:49 +0200

    Add Test ProcessorManager

--- DeSpeect/Test/View_processorhManagerTest.cpp
+++ DeSpeect/Test/View_processorhManagerTest.cpp
@@ -1,14 +1,75 @@
 #include "gtest/gtest.h"
 #include <QApplication>
 #include <processormanager.h>
-TEST(Graph, VerifyNodePaint){
+TEST(Graph, VerifyProcessorManagerIsClear){
     int argc;
     char **argv=NULL;
     QApplication app(argc,argv);
 
     ProcessorManager *p=new ProcessorManager();
 
+    p->clear();
+
     EXPECT_TRUE(p->isLayoutClean());
+
+    delete p;
+
+}
+
+TEST(Graph, VerifyProcessorManagerEvidenceNext){
+    int argc;
+    char **argv=NULL;
+    QApplication app(argc,argv);
+
+    ProcessorManager *p=new ProcessorManager();
+    QListView* qList= new QListView();
+
+    p->linkProcessorModel(qList);
+
+    p->addProcessor("");
+    p->addProcessor("a");
+    p->addProcessor("b");
+    p->addProcessor("c");
+    p->addProcessor("d");
+    p->addProcessor("e");
+
+    p->evidenceNextProcessor();
+    p->evidenceNextProcessor();
+    p->evidenceNextProcessor();
+    p->evidenceNextProcessor();
+    p->evidenceNextProcessor();
+    p->evidenceNextProcessor();
+
+    p->clearLayoutProcessor();
+
+    EXPECT_TRUE(p);
+
+    delete p;
+
+}
+
+TEST(Graph, VerifyProcessorManagerEvidenceAll){
+    int argc;
+    char **argv=NULL;
+    QApplication app(argc,argv);
+
+    ProcessorManager *p=new ProcessorManager();
+    QListView* qList= new QListView();
+
+    p->linkProcessorModel(qList);
+
+    p->addProcessor("");
+    p->addProcessor("a");
+    p->addProcessor("b");
+    p->addProcessor("c");
+    p->addProcessor("d");
+    p->addProcessor("e");
+
+    p->evidenceAllProcessor();
+
+    p->clearLayoutProcessor();
+
+    EXPECT_TRUE(p);
 
     delete p;
 
